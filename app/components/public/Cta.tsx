1ce2ddf5bb16fc533d00d2e179816feb583265b4
Joy Lodra <joylodra@gmail.com>
2023-05-01 19:37:53 +0700

    Added padding in Cta Component

--- app/components/public/Cta.tsx
+++ app/components/public/Cta.tsx
@@ -13,7 +13,7 @@ interface CtaProps {
 const Cta = ({ title, description, href, buttonText }: CtaProps) => {
   return (
     <div className="text-center bg-gradient-to-r from-blue-500 via-blue-400 to-blue-200 rounded-lg text-white">
-      <div className="mx-auto max-w-xl py-10 px-3 flex flex-col gap-6 items-center justify-center">
+      <div className="mx-auto max-w-xl py-10 px-5 flex flex-col gap-6 items-center justify-center">
         <h2 className="text-3xl font-bold font-serif">{title}</h2>
 
         <p>{description}</p>
