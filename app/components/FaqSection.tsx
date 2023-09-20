00c815c94223ad430e90b8dac47f605b24cef41c
Joy Lodra <joylodra@gmail.com>
2023-09-20 22:33:08 +0700

    Fixed map children issue and deleted sanity

--- app/components/FaqSection.tsx
+++ app/components/FaqSection.tsx
@@ -8,12 +8,12 @@ const FaqSection = () => {
   return (
     <>
       {capFAQ.map((item) => (
-        <>
+        <div key={item.answer}>
           <Accordion question={item.question} answer={item.answer} />
           <div className="px-3">
             <hr className="border border-gray-300" />
           </div>
-        </>
+        </div>
       ))}
     </>
   );
