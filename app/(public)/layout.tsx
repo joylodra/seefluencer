4796922e19c0e4410e3abaeeab9fa795f4f015a3
Joy Lodra <joylodra@gmail.com>
2025-02-03 11:04:27 +0700

    Added pixel to seefluencer.com

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -2,6 +2,7 @@
 import { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
 import dynamic from "next/dynamic";
+import Script from "next/script";
 
 // Custom Components
 import Navbar from "../components/Navbar";
@@ -38,11 +39,67 @@ export default function RootLayout({
 }) {
   return (
     <html>
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KMTTCDP8');
+            `,
+          }}
+        />
+
+        {/* Meta Pixel (Facebook Pixel) */}
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '3889155708017338');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+      </head>
+
       <body className="bg-gray-50">
+        {/* GTM (noscript fallback) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KMTTCDP8"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
+        {/* Meta Pixel (noscript fallback) */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=3889155708017338&ev=PageView&noscript=1"
+          />
+        </noscript>
+
         <Navbar />
         <main>{children}</main>
         <Footer complete />
-        <Analytics />
       </body>
     </html>
   );
